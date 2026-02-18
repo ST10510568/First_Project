80814de9d1bf0ca369f04a06557bfdb2baf14695
Jayde <Jayde@Jayden-Asus>
2026-02-18 11:45:55 +0200

    second commit

--- src/main/java/com/studyopedia/jaydensjavaproject/JaydensJavaProject.java
+++ src/main/java/com/studyopedia/jaydensjavaproject/JaydensJavaProject.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.studyopedia.jaydensjavaproject;
 
-/**
- *
- * @author Jayde
- */
 public class JaydensJavaProject {
 
     public static void main(String[] args) {
